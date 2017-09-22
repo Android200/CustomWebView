fcf77ef3ddd9cbb76412211b5ba231937fd892b5
INTRUD£R\Umar Saidu Auna <umarmanofpeace@gmail.com>
2017-09-23 00:32:34 +0100

    First Push

---
+++ app/src/main/java/minna/gdg/com/webview/MainActivity.java
@@ -0,0 +1,89 @@
+package minna.gdg.com.webview;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import java.security.PublicKey;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private String webadd = "https://github.com/Android200";
+    private ProgressBar progressBar;
+    private FrameLayout frameLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView1);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        frameLayout = (FrameLayout) findViewById(R.id.framelayout1);
+
+        webView.setWebViewClient(new CustomWebViewClient());
+
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setAppCacheMaxSize(1024 * 1024 * 8);
+        webView.getSettings().setAppCachePath(this.getCacheDir().getAbsolutePath());
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3");
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl(webadd);
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged (WebView view, int progress){
+                frameLayout.setVisibility(View.VISIBLE);
+                progressBar.setProgress(progress);
+                setTitle("Loading.....");
+
+                if(progress == 100){
+                    frameLayout.setVisibility(View.GONE);
+                    setTitle((view.getTitle()));
+                }
+                super.onProgressChanged(view,progress);
+            }
+        });
+
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+                    webView.goBack();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    //This allows for a splash screen
+    //and hide elements once the page loads
+    private class  CustomWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if(url != null && url.startsWith("http://")){
+                view.loadUrl(url);
+                frameLayout.setVisibility(View.VISIBLE);
+                view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+
+}
